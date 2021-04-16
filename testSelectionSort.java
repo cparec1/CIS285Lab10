b40ba7e611e8cefe8439694f73774e4bd75acf1b
Colin McLaughlin <cpmclaug@umich.edu>
2021-04-15 23:06:13 -0400

    files for mixed and dup

---
+++ testSelectionSort.java
@@ -0,0 +1,74 @@
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testSelectionSort {
+
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+		testMixed();
+		testDuplicates();
+	}
+	public testSelectionSort() {
+
+	}
+	public void testPositive() {
+		int[] arr=new int[5];
+		arr[0]=8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 9;
+		Sortedarr[4] = 10;
+
+	}
+	public void testNegative() {
+
+	}
+	public void testMixed() {
+    int[] arr=new int[5];
+   arr[0]=-8;
+   arr[1] = 9;
+   arr[2] = -7;
+   arr[3] = 10;
+   arr[4] = -2;
+
+   int[] Sortedarr = new int[5];
+   Sortedarr[0] = -8;
+   Sortedarr[1] = -7;
+   Sortedarr[2] = -2;
+   Sortedarr[3] = 9;
+   Sortedarr[4] = 10;
+   SelectionSort temp1=new SelectionSort();
+   int[] arraySort=temp1.basicSelectionSort(arr);
+   assertArrayEquals(arr,Sortedarr);
+
+	}
+	public void testDuplicates() {
+    int[] arr=new int[5];
+   arr[0]=8;
+   arr[1] = 8;
+   arr[2] = 0;
+   arr[3] = 0;
+   arr[4] = 3;
+
+   int[] Sortedarr = new int[5];
+   Sortedarr[0] = 0;
+   Sortedarr[1] = 0;
+   Sortedarr[2] = 3;
+   Sortedarr[3] = 8;
+   Sortedarr[4] = 8;
+   SelectionSort temp1=new SelectionSort();
+   int[] arraySort=temp1.basicSelectionSort(arr);
+   assertArrayEquals(arr,Sortedarr);
+
+	}
+}
